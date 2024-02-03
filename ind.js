e0ab8344e16a9f4e23c1f2b00911226f1e1579d4
kalpeshahire12 <158762893+kalpeshahire12@users.noreply.github.com>
2024-02-03 22:44:39 +0530

    Add files via upload

---
+++ ind.js
@@ -0,0 +1,12 @@
+const pianoKeys = document.querySelectorAll('.key')
+
+function playSound(newUrl) {
+    new Audio(newUrl).play()
+}
+
+
+pianoKeys.forEach((pianoKey, i) => {
+    
+    const newUrl = '24/key' + (i+1) + '.mp3'
+    pianoKey.addEventListener('click', () => playSound(newUrl))
+})
